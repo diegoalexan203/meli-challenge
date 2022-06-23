46be1d2d287b11c8ede4b22ed2462e9954e4d419
Diego Ochoa <diegoalexan203@gmail.com>
2022-06-23 12:56:00 -0500

    archivos faltantes

---
+++ diego8a.MeliApp/Pods/Target Support Files/Pods-diego8a.MeliApp/Pods-diego8a.MeliApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_diego8a_MeliApp : NSObject
+@end
+@implementation PodsDummy_Pods_diego8a_MeliApp
+@end
---
+++ diego8a.MeliApp/Pods/Target Support Files/Pods-diego8a.MeliAppTests/Pods-diego8a.MeliAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_diego8a_MeliAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_diego8a_MeliAppTests
+@end
